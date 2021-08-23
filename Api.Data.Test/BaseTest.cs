a52259cafa29f23cc691f7fd7bbaaf3322f18d0a
Cristiano Xavier <cristianoxc@yahoo.com.br>
2021-08-23 15:27:49 -0300

    Data.test e Service.test

---
+++ Api.Data.Test/BaseTest.cs
@@ -0,0 +1,48 @@
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+
+        public BaseTest()
+        {
+
+        }
+    }
+
+
+    //responsável por criar o banco de dados, o contexto e depois deletar tudo.
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseSqlServer($"Server=.\\;Database={dataBaseName};Trusted_Connection=True;MultipleActiveResultSets=true"),
+                  ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
